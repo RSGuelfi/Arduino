be6f6be21843f9f8796978ef9ec1f6872dc90d05
Gitnix <77456024+RSGuelfi@users.noreply.github.com>
2022-08-26 18:38:55 -0300

    Create Comentarios.ino

---
+++ Comentarios.ino
@@ -0,0 +1,15 @@
+/* 
+Arduino:
+
+- digitalWrite(pinLed, HIGH) = LIGADO
+- digitalWrite(pinLed, LOW) = DESLIGADO
+
+5v = HIGH
+0v = LOW
+- void() = FUNÇÃO
+
+- analogWrite(ledPin, value) // Valores de 0 a 1023
+
+*/
+
+Serial.Print(78) // Mostra os valores das funções em formato de leitura
